695d2cecbdd84d9d696c819f1cab9bb1dff32e1c
Ivan Akulov <p1us@Ivans-MBP.spec>
2016-06-09 10:08:09 +0500

    Two storyboards for one project (1-iPad, 2-iPhone)

---
+++ 21. iOS9Swift2.2TwoStoryboards/TwoStoryboards/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TwoStoryboards
+//
+//  Created by Ivan Akulov on 07/06/16.
+//  Copyright © 2016 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
