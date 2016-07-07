8257a502430c0a563868b50d6424d1df5c6d5f6f
Ivan Akulov <p1us@Ivans-MacBook-Pro.local>
2016-07-07 11:27:36 +0500

    Adds autolayout project

---
+++ 23. iOS9Swift2.2Autolayout/AutolayoutTut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutolayoutTut
+//
+//  Created by Ivan Akulov on 06/07/16.
+//  Copyright © 2016 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
