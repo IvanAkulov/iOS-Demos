e2aa032ecd10b46e65f485b08438167b697797bc
Ivan <iphonedev.2015@gmail.com>
2017-12-07 17:15:11 +0500

    Adds started project to 49 Speach Recognizer

---
+++ 49_starter.iOS11Swift4SpeechRecognizer/SpeechrecognitionExample/Controllers/WordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WordViewController.swift
+//  SpeechrecognitionExample
+//
+//  Created by Kostya Golenkov on 18.11.17.
+//  Copyright © 2017 Kostya Golenkov. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Speech
+
+class WordViewController: UIViewController {
+    var word = ("","")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
