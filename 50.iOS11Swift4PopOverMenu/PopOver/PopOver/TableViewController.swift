529acbda19278ac75990c4f8b932f3616d57ac18
Ivan <iphonedev.2015@gmail.com>
2018-01-22 11:45:56 +0500

    Adds popover menu demo. Menu made with UITableViewController

---
+++ 50.iOS11Swift4PopOverMenu/PopOver/PopOver/TableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  TableViewController.swift
+//  PopOver
+//
+//  Created by Ivan Akulov on 22/01/2018.
+//  Copyright © 2018 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let array = ["This is row 1",
+                 "This is row 2",
+                 "This is row 3",
+                 "This is row 4",
+                 "This is row 5"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.isScrollEnabled = false
+    }
+    
+    override func viewWillLayoutSubviews() {
+        preferredContentSize = CGSize(width: 250, height: tableView.contentSize.height)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return array.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let textData = array[indexPath.row]
+        cell.textLabel?.text = textData
+
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 50.iOS11Swift4PopOverMenu/PopOver/PopOver/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  PopOver
+//
+//  Created by Ivan Akulov on 22/01/2018.
+//  Copyright © 2018 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGestures()
+    }
+
+    private func setupGestures() {
+    
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        tapGesture.numberOfTapsRequired = 1
+        button.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func tapped() {
+        
+        guard let popVC = storyboard?.instantiateViewController(withIdentifier: "popVC") else { return }
+        
+        popVC.modalPresentationStyle = .popover
+        
+        let popOverVC = popVC.popoverPresentationController
+        popOverVC?.delegate = self
+        popOverVC?.sourceView = self.button
+        popOverVC?.sourceRect = CGRect(x: self.button.bounds.midX, y: self.button.bounds.minY, width: 0, height: 0)
+        popVC.preferredContentSize = CGSize(width: 250, height: 250)
+        
+        self.present(popVC, animated: true)
+    }
+}
+
+extension ViewController: UIPopoverPresentationControllerDelegate {
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
