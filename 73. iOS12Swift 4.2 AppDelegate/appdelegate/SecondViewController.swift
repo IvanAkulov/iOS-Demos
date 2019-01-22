05b0edb858c843d47a7c3f8c87dc191f9113df2e
Ivan <iphonedev.2015@gmail.com>
2019-01-22 12:56:37 +0500

    Adds AppDelegate demo project

---
+++ 73. iOS12Swift 4.2 AppDelegate/appdelegate/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  appdelegate
+//
+//  Created by Алексей Пархоменко on 14/01/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+    }
+}
---
+++ 73. iOS12Swift 4.2 AppDelegate/appdelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appdelegate
+//
+//  Created by Алексей Пархоменко on 14/01/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
