8526482ccc5b03f5e40b1ec50a12001b8a5a2fa8
Ivan <iphonedev.2015@gmail.com>
2020-11-11 16:24:29 +0500

    Added mask phone number demo

---
+++ 129. iOS 14 Xcode 12 MaskPhoneNumber/PhoneNumber/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  PhoneNumber
+//
+//  Created by Anastasia Sokolan on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet private weak var phoneTextField: UITextField!
+    
+    private let maxNumberCount = 11
+    private let regex = try! NSRegularExpression(pattern: "[\\+\\s-\\(\\)]", options: .caseInsensitive)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        phoneTextField.delegate = self
+        phoneTextField.keyboardType = .numberPad
+    }
+    
+    private func format(phoneNumber: String, shouldRemoveLastDigit: Bool) -> String {
+        guard !(shouldRemoveLastDigit && phoneNumber.count <= 2) else { return "+" }
+        
+        let range = NSString(string: phoneNumber).range(of: phoneNumber)
+        var number = regex.stringByReplacingMatches(in: phoneNumber, options: [], range: range, withTemplate: "")
+        
+        if number.count > maxNumberCount {
+            let maxIndex = number.index(number.startIndex, offsetBy: maxNumberCount)
+            number = String(number[number.startIndex..<maxIndex])
+        }
+        
+        if shouldRemoveLastDigit {
+            let maxIndex = number.index(number.startIndex, offsetBy: number.count - 1)
+            number = String(number[number.startIndex..<maxIndex])
+        }
+        
+        let maxIndex = number.index(number.startIndex, offsetBy: number.count)
+        let regRange = number.startIndex..<maxIndex
+        
+        if number.count < 7 {
+            let pattern = "(\\d)(\\d{3})(\\d+)"
+            number = number.replacingOccurrences(of: pattern, with: "$1 ($2) $3", options: .regularExpression, range: regRange)
+        } else {
+            let pattern = "(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d+)"
+            number = number.replacingOccurrences(of: pattern, with: "$1 ($2) $3-$4-$5", options: .regularExpression, range: regRange)
+        }
+        
+        return "+" + number
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let fullString = (textField.text ?? "") + string
+        textField.text = format(phoneNumber: fullString, shouldRemoveLastDigit: range.length == 1)
+        return false
+    }
+}
+
+// 79771773355
+// +7 (977) 177-33-55
